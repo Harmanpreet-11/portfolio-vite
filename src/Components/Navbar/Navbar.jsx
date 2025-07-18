3dd8240f2e744df14edc3d88ff5fe6975d1bbe7b
Harmanpreet Singh <harmanharjeet2003@gmail.com>
2025-07-18 10:29:17 +0530

    fixed some bugs

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 const Navbar = () => {
+
   return (
     <div className='navbar'>
-        <div className='name'>
-            <h2>Harmanpreet Singh</h2>
-        </div>
-        <div className='category'>
-            <a href="#home"><button>Home</button></a>
-            <a href="#projects"><button>Projects</button></a>
-            <a href="#education"><button>Education</button></a>
-            <a href="#about"><button>About</button></a>
-            <a href="#contact"><button>Contact</button></a>
-        </div>
+      <div className='name'>
+        <h2>Harmanpreet Singh</h2>
+      </div>
+
+
+      <div className="category">
+        <a href="#home"><button>Home</button></a>
+        <a href="#projects"><button>Projects</button></a>
+        <a href="#education"><button>Education</button></a>
+        <a href="#about"><button>About</button></a>
+        <a href="#contact"><button>Contact</button></a>
+      </div>
     </div>
   )
 }
